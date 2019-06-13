40b5b4505c03264f924a2ab08ddb16f33261460f
Kelliann Lafferty <kell10192@gmail.com>
2019-06-13 03:35:04 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,8 +4,8 @@
 def key_for_min_value(name_hash)
   low_key = nil 
   low_value = nil 
-  name_hash.each do |x, y|
-    if low_value == nil || x < low_value
+  name_hash.each do |key, value|
+    if low_value == nil || value < low_value
       low_value = y 
       low_key = x
     end 
